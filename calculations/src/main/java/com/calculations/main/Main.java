b642e8a78ababd9c65cb888a5fd4c710eefb5d41
Ojo_Rojo <famelesspeople@gmail.com>
2021-10-14 01:55:45 +0200

    Cleaning code

--- calculations/src/main/java/com/calculations/main/Main.java
+++ calculations/src/main/java/com/calculations/main/Main.java
@@ -5,10 +5,8 @@ import com.calculations.service.MultiplicationFromScratchService;
 import com.calculations.service.MultiplicationService;
 
 public class Main {
-
 	
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		MultiplicationService<?> multService = getService(args[0]);
 		String result = multService.calculate(args[1], args[2]).toString();
 		System.out.println(result);
